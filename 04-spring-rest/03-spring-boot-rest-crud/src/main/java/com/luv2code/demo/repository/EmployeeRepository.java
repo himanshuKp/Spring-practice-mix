4a8316877133d5acc3a07d6e0bffcb9ed72faaad
Himanshu Kandpal <himanshukp324@gmail.com>
2024-11-28 19:28:45 +0530

    spring data rest

--- 04-spring-rest/03-spring-boot-rest-crud/src/main/java/com/luv2code/demo/repository/EmployeeRepository.java
+++ 04-spring-rest/03-spring-boot-rest-crud/src/main/java/com/luv2code/demo/repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.luv2code.demo.repository;
 
 import com.luv2code.demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
