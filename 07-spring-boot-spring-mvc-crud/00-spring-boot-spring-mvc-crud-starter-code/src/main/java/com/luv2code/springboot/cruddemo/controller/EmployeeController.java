577774b77f483585480ef17006375dd286a519c3
Himanshu Kandpal <himanshukp324@gmail.com>
2024-12-04 13:09:16 +0530

    add employee

--- 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -4,11 +4,11 @@ import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/employees")
@@ -22,9 +22,23 @@ public class EmployeeController {
     @GetMapping("/list")
     public String getAllEmployees(Model model) {
         List<Employee> employees = employeeService.findAll();
-
+        employees = employees.stream().sorted(Comparator.comparing(Employee::getLastName)).toList();
         model.addAttribute("employees", employees);
 
         return "list-employees";
+    }
+
+    @GetMapping("/addEmployee")
+    public String addEmployeePage(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "add-employee";
+    }
+
+    @PostMapping("/add")
+    public String addEmployee(@ModelAttribute("employee") Employee employee) {
+         employeeService.save(employee);
+
+         return "redirect:/employees/list";
     }
 }
