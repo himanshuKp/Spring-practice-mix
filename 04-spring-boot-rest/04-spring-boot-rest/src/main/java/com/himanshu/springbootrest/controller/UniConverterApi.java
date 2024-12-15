a2bf65bde2b58f63c1663d440d30cf12fb1b65bb
Himanshu Kandpal <himanshukp324@gmail.com>
2024-12-15 08:55:07 +0530

    responseentity usage

--- 04-spring-boot-rest/04-spring-boot-rest/src/main/java/com/himanshu/springbootrest/controller/UniConverterApi.java
+++ 04-spring-boot-rest/04-spring-boot-rest/src/main/java/com/himanshu/springbootrest/controller/UniConverterApi.java
@@ -3,6 +3,8 @@ package com.himanshu.springbootrest.controller;
 import com.himanshu.springbootrest.customException.UnitConversionException;
 import com.himanshu.springbootrest.entity.ConversionDetails;
 import com.himanshu.springbootrest.service.UnitConverter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class UniConverterApi {
 
     @PostMapping
-    public ConversionDetails convert(@RequestBody ConversionDetails conversionDetails) throws UnitConversionException {
-        UnitConverter.convert(conversionDetails);
-        return conversionDetails;
+    public ResponseEntity<ConversionDetails> convert(@RequestBody ConversionDetails conversionDetails) {
+        try {
+            UnitConverter.convert(conversionDetails);
+        } catch (UnitConversionException e) {
+            return new ResponseEntity<>(conversionDetails, HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(conversionDetails, HttpStatus.OK);
     }
 }
