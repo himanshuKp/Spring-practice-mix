31a989bc4726a572ee9915c9bfc06e4e6684d018
Himanshu Kandpal <himanshukp324@gmail.com>
2025-01-10 00:22:13 +0530

    start and table challenge

---
+++ CoreJava/JavaPractice/src/Challenges/StarChallenge.java
@@ -0,0 +1,14 @@
+package Challenges;
+
+public class StarChallenge {
+    public static void main(String[] args) {
+        int i,j;
+
+        for(i=0;i<10;i++){
+            for(j=i;j<10;j++){
+                System.out.print('.');
+            }
+            System.out.println();
+        }
+    }
+}
