b83be41b38651957b18e3bf20db254aa5c425d88
Himanshu Kandpal <himanshukp324@gmail.com>
2025-01-26 18:31:20 +0530

    synchronized counter

---
+++ CoreJava/JavaPractice/src/synchronization/GFG2.java
@@ -0,0 +1,30 @@
+package synchronization;
+
+public class GFG2 {
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+
+        Thread thread1 = new Thread(()->{
+            for (int i = 0; i < 1000;i++) {
+                counter.increment();
+            }
+        });
+
+        Thread thread2 = new Thread(()->{
+            for (int i = 0; i < 1000; i++) {
+                counter.increment();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+        System.out.println("Counter: "+counter.getCount());
+    }
+}
