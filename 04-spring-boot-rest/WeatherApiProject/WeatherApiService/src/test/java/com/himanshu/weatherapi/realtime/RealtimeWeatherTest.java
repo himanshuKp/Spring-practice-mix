f0dd79f497c8bdaa25b184fd9d7047b5ec0577e1
Himanshu Kandpal <himanshukp324@gmail.com>
2025-02-11 08:36:33 +0530

    realtime weather repo test

---
+++ 04-spring-boot-rest/WeatherApiProject/WeatherApiService/src/test/java/com/himanshu/weatherapi/realtime/RealtimeWeatherTest.java
@@ -0,0 +1,38 @@
+package com.himanshu.weatherapi.realtime;
+
+import com.himanshu.weatherapi.common.RealtimeWeather;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.Date;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(value = false)
+public class RealtimeWeatherTest {
+    @Autowired
+    private RealtiimeWeatherRepository realtiimeWeatherRepository;
+
+    @Test
+    public void testUpdateRealtimeWeather() {
+        String location = "DELHI_IND";
+        RealtimeWeather realtimeWeather = realtiimeWeatherRepository.findById(location).isPresent() ?
+                realtiimeWeatherRepository.findById(location).get() :
+                null;
+
+        assert realtimeWeather != null;
+        realtimeWeather.setHumidity(20);
+
+        realtimeWeather.setStatus("Sunny");
+        realtimeWeather.setTemperature(80);
+        realtimeWeather.setWindSpeed(20);
+        realtimeWeather.setPrecipitation(50);
+        realtimeWeather.setLastUpdated(new Date());
+
+        RealtimeWeather updatedRealtimeWeather = realtiimeWeatherRepository.save(realtimeWeather);
+        assert updatedRealtimeWeather.getTemperature() == 80 : "Temperature should be equal to 80";
+    }
+}
