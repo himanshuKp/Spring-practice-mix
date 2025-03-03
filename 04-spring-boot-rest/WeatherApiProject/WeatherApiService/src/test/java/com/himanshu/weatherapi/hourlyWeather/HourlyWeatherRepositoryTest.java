9418346f01b019806d1f6d13d2dae49916354964
Himanshu Kandpal <himanshukp324@gmail.com>
2025-03-03 06:31:36 +0530

    junit repo

--- 04-spring-boot-rest/WeatherApiProject/WeatherApiService/src/test/java/com/himanshu/weatherapi/hourlyWeather/HourlyWeatherRepositoryTest.java
+++ 04-spring-boot-rest/WeatherApiProject/WeatherApiService/src/test/java/com/himanshu/weatherapi/hourlyWeather/HourlyWeatherRepositoryTest.java
@@ -2,6 +2,9 @@ package com.himanshu.weatherapi.hourlyWeather;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import java.lang.StackWalker.Option;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -10,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 
 import com.himanshu.weatherapi.common.HourlyWeather;
+import com.himanshu.weatherapi.common.HourlyWeatherId;
 import com.himanshu.weatherapi.common.Location;
 
 @DataJpaTest
@@ -25,16 +29,32 @@ public class HourlyWeatherRepositoryTest {
     int hourOfDay = 12;
 
     Location location = new Location().locationCode(locaionCode);
-    
+
     HourlyWeather forecast1 = new HourlyWeather().hourOfDay(hourOfDay)
-                                  .location(location)
-                                  .precipitation(30)
-                                  .temperature(50)
-                                  .status("Sunny");
+        .location(location)
+        .precipitation(30)
+        .temperature(50)
+        .status("Sunny");
 
     HourlyWeather updatedForecast = hourlyWeatherRepsitory.save(forecast1);
 
     assert updatedForecast.getHourlyWeatherId().getLocation().equals(location);
     assert updatedForecast.getHourlyWeatherId().getHourOfDay() == hourOfDay;
+  }
+  
+  @Test
+  public void testDelete() {
+    String locationCode = "DELHI_IND";
+    int hourOfDay = 12;
+
+    Location location = new Location().locationCode(locationCode);
+
+    HourlyWeatherId hourlyWeatherId = new HourlyWeatherId(hourOfDay, location);
+    
+    hourlyWeatherRepsitory.deleteById(hourlyWeatherId);
+
+    Optional<HourlyWeather> hourlyWeather = hourlyWeatherRepsitory.findById(hourlyWeatherId);
+
+    assert !hourlyWeather.isPresent();
   }
 }
